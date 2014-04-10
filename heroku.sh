bcd03eb0ced8449079c0616e1df4d10d8d3e648c
Andres Sainz de Aja <andres.sainz@globant.com>
2014-04-10 01:23:13 -0300

    update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -3,22 +3,25 @@
 HEROKU_DIR=".heroku"
 HEROKU_GIT=$1
 
+CURRENT_DIR=${PWD##*/}
+
 if [ ! -d "$HEROKU_DIR" ]; then
-  mkdir "$HEROKU_DIR"
-  echo ".heroku" >> .gitignore
-  cd "$HEROKU_DIR"
-  echo "var express = require('express');var fs = require('fs');var app = express();var read = function(filename){return fs.readFileSync(filename, 'utf-8');};app.get(/^(.*)$/, function(req, res){var uri = req.params[0];if( uri === '/' ){res.send( read('dist/index.html') );return;}uri = uri.charAt(0) === '/' ? uri.replace('/','') : uri;uri = 'dist/'+uri;if( fs.existsSync(uri) ){if( uri.indexOf('.css') > -1 ){res.setHeader('Content-Type', 'text/css');}res.send( read(uri) );return;}});app.listen(process.env.PORT || 3000);" > heroku-server.js
+  mkdir ../"$HEROKU_DIR"
+  # echo ".heroku" >> .gitignore
+  cd ../"$HEROKU_DIR"
+  git clone "$HEROKU_GIT" . -o heroku
   echo "web: node heroku-server.js" > Procfile
-  git clone "$HEROKU_GIT" -o heroku
+  echo "var express = require('express');var fs = require('fs');var app = express();var read = function(filename){return fs.readFileSync(filename, 'utf-8');};app.get(/^(.*)$/, function(req, res){var uri = req.params[0];if( uri === '/' ){res.send( read('index.html') );return;}uri = uri.charAt(0) === '/' ? uri.replace('/','') : uri;uri = ''+uri;if( fs.existsSync(uri) ){if( uri.indexOf('.css') > -1 ){res.setHeader('Content-Type', 'text/css');}res.send( read(uri) );return;}});app.listen(process.env.PORT || 3000);" > heroku-server.js
+  echo '{  "name": "herokuapp",  "version": "0.1.0",  "description": "",  "main": "heroku.js",  "scripts": {    "test": "echo \"Error: no test specified\" && exit 1"  },  "repository": {    "type": "git",    "url": ""  },  "author": "globant",  "license": "BSD-2-Clause",  "dependencies": {    "express": "~3.4.8"  }}' > package.json
   heroku login
-  cd ../
+  cd ../"$CURRENT_DIR"
 fi
 
-cd "$HEROKU_DIR"
-
 grunt build
-cp -r dist/ .heroku
+cp -r dist/ ../"$HEROKU_DIR"
+
+cd ../"$HEROKU_DIR"
 
 git add .
 git c -m "build at $(date)"
-git push
+git push heroku master
